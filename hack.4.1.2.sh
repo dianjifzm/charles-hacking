d4fef915d292adbc4b24e0edadba38a1549d6ce7
dianjifzm <dianjifzm@gmail.com>
2017-06-14 12:50:25 +0800

    Create hack.4.1.2.sh

---
+++ hack.4.1.2.sh
@@ -0,0 +1,15 @@
+charles=/Applications/Charles.app/Contents/Java/charles.jar
+dir=charleshack
+
+mkdir $dir
+cd $dir
+cat >> qFep.java <<EOF
+package com.xk72.charles;
+public final class qFep {
+	public static boolean gtOW() { return true; }
+	public static String SkgP() { return "hello myself"; }
+	public static String gtOW(String name, String key) { return null; }
+}
+EOF
+javac -encoding UTF-8 qFep.java -d .&& jar -uvf $charles com/xk72/charles/qFep.class
+cd .. && rm -rf $dir
